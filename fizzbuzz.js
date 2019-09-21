79e2d5856859a9f95427b997216f0153caa04af9
MrGrayman <gunboy.2541@gmail.com>
2019-09-21 16:15:38 +0700

    Add switch in fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,24 @@
 class FizzBuzz {
     static say(number) {
-        if (number % 3 === 0 && number % 5 === 0) {
-            return "FizzBuzz";
-        } else if (number % 5 === 0) {
-            return "Buzz";
-        } else if (number % 3 === 0) {
-            return "Fizz";
+        // if (number % 3 === 0 && number % 5 === 0) {
+        //     return "FizzBuzz";
+        // } else if (number % 5 === 0) {
+        //     return "Buzz";
+        // } else if (number % 3 === 0) {
+        //     return "Fizz";
+        // }
+        // return number;
+
+        switch (true) {
+            case number % 15 === 0:
+                return "FizzBuzz";
+            case number % 3 === 0:
+                return "Fizz";
+            case number % 5 === 0:
+                return "Buzz";
+            default:
+                return number;
         }
-        return number;
     }
 }
 
